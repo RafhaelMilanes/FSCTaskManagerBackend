312f7fddb0778e4104392fa6ea4946c58358ec9d
Rafhael Milanes <rafhael.t3ch@gmail.com>
2024-11-23 18:18:24 -0300

    chore: add cros dependec

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //importando o express
 const express = require("express");
 const dotenv = require("dotenv");
-const TaskRouter = require("./src/routes/task.routes");
+const cors = require("cors");
 
+const TaskRouter = require("./src/routes/task.routes");
 const connectToDatabase = require("./src/database/mongoose.database");
 
 dotenv.config();
 //inicializando o express
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 connectToDatabase();
